e6d701bdff16c95c8cbdc89facfdad7d679b93ea
FAxel79 <faxel79@gmail.com>
2019-06-17 20:16:51 +0300

    start commit1

--- venv/start.py
+++ venv/start.py
@@ -1,3 +1,15 @@
 import random, pygame, math
 
+run = True
+pygame.init()
+WWidht = 1500
+HHeight = 900
+win = pygame.display.set_mode((WWidht, HHeight))
+pygame.display.set_caption("Life")
 
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
